ac5844999373d57f1fdf43accb0d617fa95851c3
smit <smitpatel0045@gmail.com>
2019-02-24 17:09:55 +0530

    login form and footer added

--- app.js
+++ app.js
@@ -91,3 +91,24 @@ function manageCircles(margin, width){
   circle5.style.backgroundColor = "white";
   document.getElementById("circle" + circle).style.backgroundColor = "red";
 }
+
+//form
+const signupButton = document.getElementById('signup-button'),
+    loginButton = document.getElementById('login-button'),
+    userForms = document.getElementById('user_options-forms')
+
+/**
+ * Add event listener to the "Sign Up" button
+ */
+signupButton.addEventListener('click', () => {
+  userForms.classList.remove('bounceRight')
+  userForms.classList.add('bounceLeft')
+}, false)
+
+/**
+ * Add event listener to the "Login" button
+ */
+loginButton.addEventListener('click', () => {
+  userForms.classList.remove('bounceLeft')
+  userForms.classList.add('bounceRight')
+}, false)
